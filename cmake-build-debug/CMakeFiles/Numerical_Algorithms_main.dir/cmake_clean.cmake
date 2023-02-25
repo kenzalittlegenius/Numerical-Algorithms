aac4ea6c138e113e36a5ccc7097989b45b337b81
Kenza Benmansour <35221608+kenzalittlegenius@users.noreply.github.com>
2023-02-25 12:07:01 -0500

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/Numerical_Algorithms_main.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Numerical_Algorithms_main.dir/TP1/TP1-IFT2425-I.1.c.o"
+  "Numerical_Algorithms_main"
+  "Numerical_Algorithms_main.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Numerical_Algorithms_main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
